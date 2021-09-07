925653f49139fb33e0dd35f9ee92c0151aa67489
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2021-09-07 01:12:10 -0400

    Make mousewheel work

--- components/cards.tsx
+++ components/cards.tsx
@@ -3,7 +3,9 @@ import { Grid, makeStyles } from '@material-ui/core';
 import ImageCard from './image-card';
 import clsx from 'clsx';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import "swiper/css";
+import { Mousewheel } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/mousewheel';
 
 const useStyles = makeStyles(theme => ({
   cards: {
@@ -24,7 +26,13 @@ const Cards: FC<CardsProps> = ({ className }) => {
   const styles = useStyles();
   return (
     <Grid container item className={clsx(styles.cards, className)}>
-      <Swiper direction="vertical" className={styles.swiper}>
+      <Swiper
+        direction="vertical"
+        mousewheel={{ releaseOnEdges: true }}
+        className={styles.swiper}
+        modules={[Mousewheel]}
+        scrollbar={{ draggable: true }}
+      >
       {Array(9).fill(0).map((_, i) =>
         <SwiperSlide key={i}>
           <ImageCard />
